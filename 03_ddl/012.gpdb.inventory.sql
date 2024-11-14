dcda73f3f9ce74cad30c8778bbc071f4c0158638
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2024-11-14 16:52:47 +0300

    Update 012.gpdb.inventory.sql

--- 03_ddl/012.gpdb.inventory.sql
+++ 03_ddl/012.gpdb.inventory.sql
@@ -4,4 +4,8 @@ CREATE TABLE tpcds.inventory (
     inv_warehouse_sk integer NOT NULL,
     inv_quantity_on_hand integer
 )
-DISTRIBUTED RANDOMLY;
+WITH (:LARGE_STORAGE)
+:DISTRIBUTED_BY
+partition by range(inv_date_sk)
+(start(2450815) INCLUSIVE end(2453005) INCLUSIVE every (100),
+default partition others);
