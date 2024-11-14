ff4bd5c8f76e42e8d29522b5294e8583b2a03ef8
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2024-11-14 16:53:33 +0300

    Update 015.gpdb.reason.sql

--- 03_ddl/015.gpdb.reason.sql
+++ 03_ddl/015.gpdb.reason.sql
@@ -3,4 +3,5 @@ CREATE TABLE tpcds.reason (
     r_reason_id character varying(16) NOT NULL,
     r_reason_desc character varying(100)
 )
-DISTRIBUTED RANDOMLY;
+WITH (:SMALL_STORAGE)
+:DISTRIBUTED_BY;
