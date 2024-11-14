bf96dd58955b38591b86d890584df325db7fe810
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2024-11-14 16:54:27 +0300

    Update 019.gpdb.store_sales.sql

--- 03_ddl/019.gpdb.store_sales.sql
+++ 03_ddl/019.gpdb.store_sales.sql
@@ -23,4 +23,8 @@ CREATE TABLE tpcds.store_sales (
     ss_net_paid_inc_tax numeric(7,2),
     ss_net_profit numeric(7,2)
 )
-DISTRIBUTED RANDOMLY;
+WITH (:LARGE_STORAGE)
+:DISTRIBUTED_BY
+PARTITION BY RANGE (ss_sold_date_sk)
+(start(2450815) INCLUSIVE end(2453005) INCLUSIVE every (10),
+default partition others);
