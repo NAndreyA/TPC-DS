91c3034614d0b6edf7ddb4209de40adaaa511632
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-12 18:49:09 +0300

    Update tpcds.sh

--- tpcds.sh
+++ tpcds.sh
@@ -125,7 +125,7 @@ check_variables()
  	#10
 	local count=$(grep "RUN_OS" $MYVAR | wc -l)
 	if [ "$count" -eq "0" ]; then
-		echo "RUN_OS=\"Astra Linux\"" >> $MYVAR
+		echo "RUN_OS=\"RED OS\"" >> $MYVAR
   		#echo "*OS: RED OS, Astra Linux, ALT SP Server" >> $MYVAR
 		new_variable=$(($new_variable + 1))
 	fi
