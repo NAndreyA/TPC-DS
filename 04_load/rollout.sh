b1a8a2a4e69484d9de0b3550712d3e0ac7ab1e4b
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-14 17:02:20 +0300

    Update rollout.sh

--- 04_load/rollout.sh
+++ 04_load/rollout.sh
@@ -5,7 +5,7 @@ PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $PWD/../functions.sh
 source_bashrc
 
-echo "'$RUN_COMPRESS' and '$LEVEL_COMPRESS'"
+echo "$RUN_COMPRESS and $LEVEL_COMPRESS and $RUN_DDL and $RUN_OS"
 
 step=load
 init_log $step
