8eda068a10202aa308a5a59535530ac308f62c16
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-21 17:16:27 +0300

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -55,33 +55,23 @@ get_version()
 {
 	#need to call source_bashrc first
 	VERSION=$(psql -v ON_ERROR_STOP=1 -t -A -c "SELECT CASE WHEN POSITION ('Greenplum Database 4.3' IN version) > 0 THEN 'gpdb_4_3' WHEN POSITION ('Greenplum Database 5' IN version) > 0 THEN 'gpdb_5' WHEN POSITION ('Greenplum Database 6' IN version) > 0 THEN 'gpdb_6' ELSE 'postgresql' END FROM version();") 
-	if [[ "$VERSION" == *"gpdb"* ]]; then
+	if [[ "$VERSION" == *"gpdb"* && "$TYPE_ORIENTATION" = "row" -o "$TYPE_ORIENTATION" = "column" ]]; then
 		compress_test=$(psql -v ON_ERROR_STOP=1 -t -A -c "SELECT COUNT(*) FROM pg_compression WHERE compname = LOWER('$TYPE_COMPRESS')")
 		if [ "$compress_test" -eq "1" ]; then
   			if [ "$TYPE_ORIENTATION" = "row" -o "$TYPE_ORIENTATION" = "column" ]; then
    				SMALL_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\", compresstype=\"$TYPE_COMPRESS\", compresslevel=\"$LEVEL_COMPRESS\""
 				MEDIUM_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\", compresstype=\"$TYPE_COMPRESS\", compresslevel=\"$LEVEL_COMPRESS\""
 	 			LARGE_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\", compresstype=\"$TYPE_COMPRESS\", compresslevel=\"$LEVEL_COMPRESS\""
-			else
-				SMALL_STORAGE="compresstype=\"$TYPE_COMPRESS\", compresslevel=\"$LEVEL_COMPRESS\""
-				MEDIUM_STORAGE="compresstype=\"$TYPE_COMPRESS\", compresslevel=\"$LEVEL_COMPRESS\""
-				LARGE_STORAGE="compresstype=\"$TYPE_COMPRESS\", compresslevel=\"$LEVEL_COMPRESS\""
-  			fi
-			#SMALL_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
-			#MEDIUM_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
-	 		#LARGE_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
-		else
-    			if [ "$TYPE_ORIENTATION" = "row" -o "$TYPE_ORIENTATION" = "column" ]; then
-   				SMALL_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
-				MEDIUM_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
-	 			LARGE_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
-			else
-				SMALL_STORAGE=""
-				MEDIUM_STORAGE=""
-				LARGE_STORAGE=""
-  			fi
-     		fi
-
+			fi
+   		else
+   			SMALL_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
+			MEDIUM_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
+	 		LARGE_STORAGE="appendonly=true, orientation=\"$TYPE_ORIENTATION\""
+  		fi
+    	else
+		SMALL_STORAGE=""
+		MEDIUM_STORAGE=""
+		LARGE_STORAGE=""
 	fi
 }
 init_log()
