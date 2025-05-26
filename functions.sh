25adefb25a8c2eed8f1c15be18d611e736bdbbc5
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-26 19:57:50 +0300

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -55,7 +55,7 @@ get_version()
 {
 	#need to call source_bashrc first
 	VERSION=$(psql -v ON_ERROR_STOP=1 -t -A -c "SELECT CASE WHEN POSITION ('Greenplum Database 4.3' IN version) > 0 THEN 'gpdb_4_3' WHEN POSITION ('Greenplum Database 5' IN version) > 0 THEN 'gpdb_5' WHEN POSITION ('Greenplum Database 6' IN version) > 0 THEN 'gpdb_6' ELSE 'postgresql' END FROM version();") 
-	if [[ "$VERSION" == *"gpdb"* ]] && [[ "$TYPE_ORIENTATION" == "row" || "$TYPE_ORIENTATION" == "column" ]]; then
+	if [[ "$VERSION" == *"gpdb"* ]] && [[ "$TYPE_ORIENTATION" = "row" || "$TYPE_ORIENTATION" = "column" ]]; then
  		echo "functions.sh -> get_version -> $VERSION and $TYPE_ORIENTATION"
 		compress_test=$(psql -v ON_ERROR_STOP=1 -t -A -c "SELECT COUNT(*) FROM pg_compression WHERE compname = LOWER('$TYPE_COMPRESS')")
 		if [ "$compress_test" -eq "1" ]; then
