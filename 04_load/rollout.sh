33293a846a5ec980944f527b1d3b3e41181456c8
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-28 14:56:58 +0300

    Update rollout.sh

--- 04_load/rollout.sh
+++ 04_load/rollout.sh
@@ -15,7 +15,7 @@ init_log $step
 ADMIN_HOME=$(eval echo ~$ADMIN_USER)
 
 get_version
-if [[ "$VERSION" == *"gpdb"* ]] && [["$VERSION1" == "postgresql"]]; then
+if [[ "$VERSION" == *"gpdb"* ]] || [["$VERSION1" == "postgresql"]]; then
 	filter="gpdb"
 elif [ "$VERSION" == "postgresql" ]; then
 	filter="postgresql"
