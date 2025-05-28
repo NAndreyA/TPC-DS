27a5a6470d5f538fd0076cd90b134ffd11f5cf90
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-28 18:24:13 +0300

    Update 024.gpdb_postgresql.web_sales.sql

--- 03_ddl/024.gpdb_postgresql.web_sales.sql
+++ 03_ddl/024.gpdb_postgresql.web_sales.sql
@@ -35,4 +35,4 @@ CREATE TABLE tpcds.web_sales (
     ws_net_profit numeric(7,2)
 );
 
-alter table tpcds.web_sales add primary key (ws_item_sk, ws_order_number, ws_ext_sales_price);
+alter table tpcds.web_sales add primary key (ws_item_sk, ws_order_number, ws_wholesale_cost, ws_coupon_amt);
