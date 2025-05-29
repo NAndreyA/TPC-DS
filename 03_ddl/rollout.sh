d8de75986342f6e47536c54ead104219768f57a1
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-29 17:45:46 +0300

    Update rollout.sh

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -35,10 +35,13 @@ echo "03_ddl -> rollout.sh -> $VERSION and $VERSION1"
 if [[ "$VERSION" == *"gpdb"* && "$VERSION1" == "gpdb_postgresql" ]]; then
 #if [[ "$VERSION1" == "postgresql" ]]; then
 	filter="gpdb_postgresql"
+ 	echo "03_ddl -> rollout.sh -> filter = $filter"
 elif [[ "$VERSION" == *"gpdb"* && "$VERSION1" == "null" ]]; then
 	filter="gpdb"
+ 	echo "03_ddl -> rollout.sh -> filter = $filter"
 elif [ "$VERSION" == "postgresql" ]; then
 	filter="postgresql"
+ 	echo "03_ddl -> rollout.sh -> filter = $filter"
 else
 	echo "ERROR: Unsupported VERSION $VERSION!"
 	exit 1
