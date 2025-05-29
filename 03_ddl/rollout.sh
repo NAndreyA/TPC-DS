affbcb67c721b9127753b3df55043ca2e6b2cf63
NAndreyA <130079379+NAndreyA@users.noreply.github.com>
2025-05-29 16:56:40 +0300

    Update rollout.sh

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -32,8 +32,8 @@ get_version
 
 echo "03_ddl -> rollout.sh -> $VERSION and $VERSION1"
 
-#if [[ "$VERSION" == *"gpdb"* && "$VERSION1" == "postgresql" ]]; then
-if [[ "$VERSION1" == "postgresql" ]]; then
+if [[ "$VERSION" == *"gpdb"* && "$VERSION1" == "postgresql" ]]; then
+#if [[ "$VERSION1" == "postgresql" ]]; then
 	filter="gpdb_postgresql"
 elif [ "$VERSION" == *"gpdb"* ]; then
 	filter="gpdb"
